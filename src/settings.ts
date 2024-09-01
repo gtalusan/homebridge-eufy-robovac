bbf141815e3cc7e48a9f775589d807dd780b2c44
george <george.talusan@gmail.com>
2024-08-31 21:55:34 -0400

    update urls/meta

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'EufyRobovacHomebridgePlugin';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-plugin-eufy-robovac';
+export const PLUGIN_NAME = '@george.talusan/homebridge-eufy-robovac';
